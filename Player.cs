544d724270fd078e73fe8ecb40c0d3921a1fc4fb
Luzbella Di Alba <luzbella.inbox@gmail.com>
2024-03-13 12:02:09 -0400

    added camara motion

--- Player.cs
+++ Player.cs
@@ -1,12 +1,38 @@
+using System.Reflection.Metadata;
 using System.Runtime.Serialization;
 using Godot;
 
 public partial class Player : CharacterBody3D
 {
     [Export]
-    public int speed {get; set;} = 20;
+    public int Speed {get; set;} = 20;
     [Export]
     public int jump {get; set;} = 24;
+    
+    private double mouse_sens = 0.3f;
+    private double camara_angle = 0;
+    private int _gravity = 9;
+
+    public Node3D head;
+    public Camera3D camara;
+
+    private Vector3 _targetVelocity = Vector3.Zero;
+
+    public override void _Ready()
+    {
+        head = GetNode<Node3D>("Pivot");
+        camara = GetNode<Camera3D>("Pivot/UIView");
+    }
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion)
+        {
+            InputEventMouseMotion mouseMotion = @event as InputEventMouseMotion;
+            head.RotateY(-mouseMotion.Relative.X * 0.01f);
+            camara.RotateX(-mouseMotion.Relative.Y * 0.01f);
+
+        }
+    }
 
     public override void _PhysicsProcess(double delta)
     {
@@ -16,11 +42,11 @@ public partial class Player : CharacterBody3D
 
         if (Input.IsActionPressed("move_up")) 
         {
-            direction.Y += 1.0f;
+            direction.Z -= 1.0f;
         }
-        if (Input.IsActionPressed("move_down")) 
+        if (Input.IsActionPressed("move_back")) 
         {
-            direction.Y -= 1.0f;
+            direction.Z += 1.0f;
         }
         if (Input.IsActionPressed("move_right")) 
         {
@@ -31,7 +57,23 @@ public partial class Player : CharacterBody3D
             direction.X -= 1.0f;
         }
 
-        
+
+        if (direction != Vector3.Zero)
+        {
+            direction = direction.Normalized();
+            //GetNode<Node3D>("Pivot").Basis = Basis.LookingAt(direction);
+        }
+
+        _targetVelocity.X =  direction.X * Speed;
+        _targetVelocity.Z =  direction.Z * Speed;
+
+        if (!IsOnFloor())
+        {
+            _targetVelocity.Y -= _gravity * (float)delta;
+        }
+
+        Velocity =  _targetVelocity;
+        MoveAndSlide();
 
     }
 }
